74731c538deec3d56d51c39f8ff1b01b2645878a
kou30 <krbdkou@gmail.com>
2023-09-25 15:01:05 +0900

    aaa

---
+++ src/main/java/schedule/DeleteCheck2.java
@@ -0,0 +1,313 @@
+package schedule;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Calendar;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+	@WebServlet("/DeleteCheck2")
+	public class DeleteCheck2 extends HttpServlet {
+	    protected Connection conn = null;
+
+	    public void init() throws ServletException {
+	        String DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
+	        String JDBC_URL = "jdbc:mysql://192.168.1.21/forest_db?characterEncoding=UTF-8&useSSL=false";
+	        String USER_ID = "forest_user";
+	        String USER_PASS = "forest_pass";
+
+	        try {
+	            Class.forName(DRIVER_NAME);
+	            conn = DriverManager.getConnection(JDBC_URL, USER_ID, USER_PASS);
+	        } catch (ClassNotFoundException e) {
+	            log("ClassNotFoundException: " + e.getMessage());
+	        } catch (SQLException e) {
+	            log("SQLException: " + e.getMessage());
+	        } catch (Exception e) {
+	            log("Exception: " + e.getMessage());
+	            e.printStackTrace();
+	            throw new ServletException("初期化エラー: " + e.getMessage(), e);
+	        }
+	    }
+
+	    
+	    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+	        req.setCharacterEncoding("UTF-8"); // リクエストの文字エンコーディングを設定
+	        res.setCharacterEncoding("UTF-8"); // レスポンスの文字エンコーディングを設定
+	        PrintWriter out = res.getWriter();
+
+        int year = -1;
+        int month = -1;
+        int day = -1;
+        int currentscheduleid;
+        String currentStartTime = "";
+        String currentEndTime = "";
+        String currentSchedule = "";
+        String currentMemo = "";
+
+        String param = req.getParameter("ID");
+        if (param == null || param.length() == 0){
+            currentscheduleid = -1;
+        }else{
+            try{
+                currentscheduleid = Integer.parseInt(param);
+            }catch (NumberFormatException e){
+                currentscheduleid = -1;
+            }
+        }
+
+        /* パラメータが不正な場合はトップページへリダイレクト */
+        if (currentscheduleid == -1){
+            res.sendRedirect("top.html");
+        }
+
+        /* ユーザー情報を取り出す */
+        HttpSession session = req.getSession(false);
+        String username = (String)session.getAttribute("username");
+        String tmpuserid = (String)session.getAttribute("userid");
+        int userid = 0;
+        if (tmpuserid != null){
+            userid = Integer.parseInt(tmpuserid);
+        }
+
+        try {
+            String sql = "SELECT * FROM schedule WHERE id = ?";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+
+            pstmt.setInt(1, currentscheduleid);
+            ResultSet rs = pstmt.executeQuery();
+
+            rs.next();
+            String scheduledate = rs.getString("scheduledate");
+            String yearStr = scheduledate.substring(0, 4);
+            String monthStr = scheduledate.substring(5, 7);
+            String dayStr = scheduledate.substring(8, 10);
+
+            year = Integer.parseInt(yearStr);
+            month = Integer.parseInt(monthStr) - 1;
+            day = Integer.parseInt(dayStr);
+
+            currentStartTime = rs.getString("starttime");
+            currentEndTime = rs.getString("endtime");
+            currentSchedule = rs.getString("schedule");
+            currentMemo = rs.getString("schedulememo");
+
+            rs.close();
+            pstmt.close();
+
+        }catch (SQLException e){
+            log("SQLException:" + e.getMessage());
+        }
+
+        StringBuffer sb = new StringBuffer();
+
+        sb.append("<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.0.1//EN\" \"http://www.w3.org/TR/html4/strict.dtd\">");
+
+        sb.append("<html lang=\"ja\">");
+        sb.append("<head>");
+        sb.append("<meta http-equiv=\"Content-Type\" Content=\"text/html;charset=UTF-8\">");
+
+        sb.append("<title>スケジュール削除</title>");
+
+        sb.append("<style>");
+        sb.append("table.sche{border:1px solid #a9a9a9;padding:0px;margin:0px;border-collapse:collapse;}");
+        sb.append("td{vertical-align:top;margin:0px;padding:2px;font-size:0.75em;height:20px;}");
+        sb.append("td.top{border-bottom:1px solid #a9a9a9;text-align:center;}");
+        sb.append("td.time{background-color:#f0f8ff;text-align:right;border-right:1px double #a9a9a9;padding-right:5px;}");
+        sb.append("td.timeb{background-color:#f0f8ff;border-bottom:1px solid #a9a9a9;border-right:1px double #a9a9a9;}");
+        sb.append("td.contents{background-color:#ffffff;border-bottom:1px dotted #a9a9a9;}");
+        sb.append("td.contentsb{background-color:#ffffff;border-bottom:1px solid #a9a9a9;}");
+        sb.append("td.ex{background-color:#ffebcd;border:1px solid #8b0000;}");
+        sb.append("table.view{border:1px solid #a9a9a9;padding:0px;margin:0px;border-collapse:collapse;width:250px}");
+        sb.append("table.view td{border:1px solid #a9a9a9;}");
+        sb.append("table.view td.left{width:70px;background-color:#f0f8ff;}");
+        sb.append("img{border:0px;}");
+        sb.append("p{font-size:0.75em;}");
+        sb.append("</style>");
+
+        sb.append("</head>");
+        sb.append("<body>");
+
+        sb.append("<p>");
+        sb.append(username);
+        sb.append("さんのスケジュールです");
+        sb.append("</p>");
+
+        sb.append("<p>");
+        sb.append("スケジュールの削除確認  ");
+        sb.append("[<a href=\"ScheduleView2");
+        sb.append("?ID=");
+        sb.append(currentscheduleid);
+        sb.append("\">スケジュール表示へ戻る</a>]");
+        sb.append("</p>");
+
+        String[] scheduleArray = new String[49];
+        int[] widthArray = new int[49];
+
+        for (int i = 0 ; i < 49 ; i++){
+            scheduleArray[i] = "";
+            widthArray[i] = 0;
+        }
+
+        try {
+            String sql = "SELECT * FROM schedule WHERE userid = ? and scheduledate = ? ORDER BY starttime";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+
+            String startDateStr = year + "-" + (month + 1) + "-" + day;
+            pstmt.setInt(1, userid);
+            pstmt.setString(2, startDateStr);
+
+            ResultSet rs = pstmt.executeQuery();
+
+            while(rs.next()){
+                int id = rs.getInt("id");
+                String starttime = rs.getString("starttime");
+                String endtime = rs.getString("endtime");
+                String schedule = rs.getString("schedule");
+
+                if (starttime == null || endtime == null){
+                    widthArray[0] = 1;
+
+                    StringBuffer sbSchedule = new StringBuffer();
+                    sbSchedule.append("<a href=\"ScheduleView2?ID=");
+                    sbSchedule.append(id);
+                    sbSchedule.append("\">");
+                    sbSchedule.append(schedule);
+                    sbSchedule.append("</a>");
+
+                    scheduleArray[0] = scheduleArray[0] + (new String(sbSchedule)) + "<br>";
+
+                }else{
+                    String startTimeStr = starttime.substring(0, 2);
+                    String startMinuteStr = starttime.substring(3, 5);
+
+                    int startTimeNum = Integer.parseInt(startTimeStr);
+                    int index = startTimeNum * 2 + 1;
+                    if (startMinuteStr.equals("30")){
+                        index++;
+                    }
+
+                    if (widthArray[index] == 0){
+                    /* 既にデータが入っていた場合は異常な状態なので無視する */
+
+                        String endTimeStr = endtime.substring(0, 2);
+                        String endMinuteStr = endtime.substring(3, 5);
+
+                        int endTimeNum = Integer.parseInt(endTimeStr);
+                        int width = (endTimeNum - startTimeNum) * 2;
+
+                        if (startMinuteStr.equals("30")){
+                            width--;
+                        }
+
+                        if (endMinuteStr.equals("30")){
+                            width++;
+                        }
+
+                        StringBuffer sbSchedule = new StringBuffer();
+                        sbSchedule.append(startTimeStr);
+                        sbSchedule.append(":");
+                        sbSchedule.append(startMinuteStr);
+                        sbSchedule.append("-");
+                        sbSchedule.append(endTimeStr);
+                        sbSchedule.append(":");
+                        sbSchedule.append(endMinuteStr);
+                        sbSchedule.append(" ");
+                        sbSchedule.append("<a href=\"ScheduleView2?ID=");
+                        sbSchedule.append(id);
+                        sbSchedule.append("\">");
+                        sbSchedule.append(schedule);
+                        sbSchedule.append("</a>");
+
+                        scheduleArray[index] = new String(sbSchedule);
+                        widthArray[index] = width;
+
+                        /* 同じスケジュールの先頭以外の箇所には「-1」を設定 */
+                        for (int i = 1 ; i < width ; i++){
+                            widthArray[index + i] = -1;
+                        }
+                    }
+                }
+            }
+
+            rs.close();
+            pstmt.close();
+
+        }catch (SQLException e){
+            log("SQLException:" + e.getMessage());
+        }
+
+        sb.append("<table class=\"view\">");
+        sb.append("<tr><td class=\"left\">日付</td><td>");
+        sb.append(year);
+        sb.append("年");
+        sb.append(month + 1);
+        sb.append("月");
+        sb.append(day);
+        sb.append("日");
+        sb.append("</td></tr>");
+        sb.append("<tr><td class=\"left\">時間</td><td>");
+        if (currentStartTime == null){
+            sb.append("未定");
+        }else{
+            sb.append(currentStartTime.substring(0, 5));
+            sb.append(" - ");
+            sb.append(currentEndTime.substring(0, 5));
+        }
+        sb.append("</td></tr>");
+        sb.append("<tr><td class=\"left\">スケジュール</td><td>");
+        sb.append(currentSchedule);
+        sb.append("</td></tr>");
+        sb.append("<tr><td class=\"left\" style=\"height:150px;\">メモ</td><td>");
+        currentMemo = currentMemo.replaceAll("\r\n", "<br>");
+        sb.append(currentMemo);
+        sb.append("</td></tr>");
+        sb.append("</table>");
+
+        sb.append("<p>スケジュールを削除します。一度削除すると元には戻せません</p>");
+        sb.append("<p>削除しますか？</p>");
+
+        sb.append("<p>");
+        sb.append("[<a href=\"ScheduleDelete1?ID=");
+        sb.append(currentscheduleid);
+        sb.append("&YEAR=");
+        sb.append(year);
+        sb.append("&MONTH=");
+        sb.append(month);
+        sb.append("\">削除する</a>]");
+        sb.append("  ");
+        sb.append("[<a href=\"ScheduleView2?ID=");
+        sb.append(currentscheduleid);
+        sb.append("\">キャンセル</a>]");
+        sb.append("</p>");
+
+        sb.append("</body>");
+        sb.append("</html>");
+
+        out.println(new String(sb));
+    }
+
+    protected int getMonthLastDay(int year, int month, int day){
+
+        Calendar calendar = Calendar.getInstance();
+
+        /* 今月が何日までかを確認する */
+        calendar.set(year, month + 1, 0);
+        int thisMonthlastDay = calendar.get(Calendar.DATE);
+
+        return thisMonthlastDay;
+    }
+
+}
---
+++ src/main/java/schedule/ScheduleInsert3.java
@@ -0,0 +1,208 @@
+
+package schedule;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@WebServlet("/ScheduleInsert3")
+public class ScheduleInsert3 extends HttpServlet {
+    protected Connection conn = null;
+
+    public void init() throws ServletException {
+        String DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
+        String JDBC_URL = "jdbc:mysql://192.168.1.21/forest_db?characterEncoding=UTF-8&useSSL=false";
+        String USER_ID = "forest_user";
+        String USER_PASS = "forest_pass";
+
+        try {
+            Class.forName(DRIVER_NAME);
+            conn = DriverManager.getConnection(JDBC_URL, USER_ID, USER_PASS);
+        } catch (ClassNotFoundException e) {
+            log("ClassNotFoundException: " + e.getMessage());
+        } catch (SQLException e) {
+            log("SQLException: " + e.getMessage());
+        } catch (Exception e) {
+            log("Exception: " + e.getMessage());
+            e.printStackTrace();
+            throw new ServletException("初期化エラー: " + e.getMessage(), e);
+        }
+    }
+
+    
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+        req.setCharacterEncoding("UTF-8"); // リクエストの文字エンコーディングを設定
+        res.setCharacterEncoding("UTF-8"); // レスポンスの文字エンコーディングを設定
+
+        PrintWriter out = res.getWriter();
+
+        int year;
+        int month;
+        int day;
+        int shour;
+        int sminute;
+        int ehour;
+        int eminute;
+        String plan;
+        String memo;
+
+        String param = req.getParameter("YEAR");
+        if (param == null || param.length() == 0){
+            year = -999;
+        }else{
+            try{
+                year = Integer.parseInt(param);
+            }catch (NumberFormatException e){
+                year = -999;
+            }
+        }
+
+        param = req.getParameter("MONTH");
+        if (param == null || param.length() == 0){
+            month = -999;
+        }else{
+            try{
+                month = Integer.parseInt(param);
+            }catch (NumberFormatException e){
+                month = -999;
+            }
+        }
+
+        param = req.getParameter("DAY");
+        if (param == null || param.length() == 0){
+            day = -999;
+        }else{
+            try{
+                day = Integer.parseInt(param);
+            }catch (NumberFormatException e){
+                day = -999;
+            }
+        }
+
+        param = req.getParameter("SHOUR");
+        if (param == null || param.length() == 0){
+            shour = -999;
+        }else{
+            try{
+                shour = Integer.parseInt(param);
+            }catch (NumberFormatException e){
+                shour = -999;
+            }
+        }
+
+        param = req.getParameter("SMINUTE");
+        if (param == null || param.length() == 0){
+            sminute = -999;
+        }else{
+            try{
+                sminute = Integer.parseInt(param);
+            }catch (NumberFormatException e){
+                sminute = -999;
+            }
+        }
+
+        param = req.getParameter("EHOUR");
+        if (param == null || param.length() == 0){
+            ehour = -999;
+        }else{
+            try{
+                ehour = Integer.parseInt(param);
+            }catch (NumberFormatException e){
+                ehour = -999;
+            }
+        }
+
+        param = req.getParameter("EMINUTE");
+        if (param == null || param.length() == 0){
+            eminute = -999;
+        }else{
+            try{
+                eminute = Integer.parseInt(param);
+            }catch (NumberFormatException e){
+                eminute = -999;
+            }
+        }
+
+        param = req.getParameter("PLAN");
+        if (param == null || param.length() == 0){
+            plan = "";
+        }else{
+            try{
+                plan = param;
+            }catch (NumberFormatException e){
+                plan = "";
+            }
+        }
+
+        param = req.getParameter("MEMO");
+        if (param == null || param.length() == 0){
+            memo = "";
+        }else{
+            try{
+                memo = param;
+            }catch (NumberFormatException e){
+                memo = "";
+            }
+        }
+
+        /* 日付が不正な値で来た場合はパラメータ無しで「MonthView」へリダイレクトする */
+        if (year == -999 || month == -999 || day == -999){
+            res.sendRedirect("MonthView7");
+        }
+        String dateStr = year + "-" + month + "-" + day;
+
+        String startTimeStr = shour + ":" + sminute + ":00";
+        String endTimeStr = ehour + ":" + eminute + ":00";
+        /* 日付が指定されていない場合は、開始及び終了時刻をNULLとして登録する */
+        if (shour == -999 || sminute == -999 || ehour == -999 || eminute == -999){
+            startTimeStr = null;
+            endTimeStr = null;
+        }
+
+        /* ユーザー情報を取り出す */
+        HttpSession session = req.getSession(false);
+        String tmpuserid = (String)session.getAttribute("userid");
+        int userid = 0;
+        if (tmpuserid != null){
+            userid = Integer.parseInt(tmpuserid);
+        }
+
+        try {
+            String sql = "insert into schedule (userid, scheduledate, starttime, endtime, schedule, schedulememo) values (?, ?, ?, ?, ?, ?)";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+
+            pstmt.setInt(1, userid);
+            pstmt.setString(2, dateStr);
+            pstmt.setString(3, startTimeStr);
+            pstmt.setString(4, endTimeStr);
+            pstmt.setString(5, plan);
+            pstmt.setString(6, memo);
+            int num = pstmt.executeUpdate();
+
+            pstmt.close();
+
+        }catch (SQLException e){
+            out.println("SQLException:" + e.getMessage());
+        }
+
+        StringBuffer sb = new StringBuffer();
+        sb.append("MonthView7");
+        sb.append("?YEAR=");
+        sb.append(year);
+        sb.append("&MONTH=");
+        sb.append(month - 1);
+        res.sendRedirect(new String(sb));
+    }
+}
